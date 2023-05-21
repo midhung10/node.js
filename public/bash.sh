2345c5286ecf76ade9c55062cdb5f1b0dbb22e94
ss <ss@gmail.com>
2023-05-21 18:15:52 +0530

    test

--- public/bash.sh
+++ public/bash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "this a test from project folde rfgvftesfdsfting polfefeffefl scmfefe frf testing github fdf"
+echo "thfjdnvjnis a test from project folde rfgvftesfdsfting polfefeffefl scmfefe frf testing github fdf"
