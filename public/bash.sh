c6e46aac0638e9e2e3a4dcd088c228ea36bb966e
Midhun G <63657060+midhung10@users.noreply.github.com>
2023-05-22 10:22:09 +0530

    Update bash.sh

--- public/bash.sh
+++ public/bash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "thfjdnvjnis a test frrgsfgom project fefvdolde rfrfrffrtrtfgvftesfdsfting polfefeffefl scmffefefefererefe frf testing github fdf"
+echo "thfjdnvjnis a test frrgsfgom project fefvdolde rfrfrffrtrtfgvftesfdsfting polfefeffefjjjjl scmffefefefererefe frf testing github fdf"
