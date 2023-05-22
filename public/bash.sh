fb69cf2e9d3597f274f009cc13070f47b24f81bd
Midhun g <gmidhun10@gmail.com>
2023-05-22 12:13:19 +0530

    test

--- public/bash.sh
+++ public/bash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "thfjdhajsdbfhsognjndjfnjsnfjbnvjnis a testfefe frrgsfgom project fefvdolde rfrfrffrtrtffffffgvftrrffesfdsfting polfedddddddfeffefjjjjl scmffefefeerererererfererefe frf testing github fdf"
+echo "thfjdhajsdbfhsognjndjfnjsnfjbnvjnissdnfinsfnhs a testfefe frrgsfgom project fefvdolde rfrfrffrtrtffffffgvftrrffesfdsfting polfedddddddfeffefjjjjl scmffefefeerererererfererefe frf testing github fdf"
